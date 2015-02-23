b557ea4b09f42034dfe4f7805f00005ce61a10d4
kb10uy <kb10uy@live.jp>
2015-02-24 00:07:05 +0900

    bootstrap-sassに差し戻しても動いた

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'coffee-rails', '~> 4.1.0'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
+gem 'jquery-ui-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
@@ -35,7 +36,7 @@ gem 'omniauth-twitter'
 gem 'figaro'
 gem 'haml-rails'
 gem 'acts-as-taggable-on'
-#gem 'bootstrap-sass', '~>3.3.3'
+gem 'bootstrap-sass', '~>3.3.3'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,8 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( bootstrap/glyphicons-halflings-regular.eot )
-Rails.application.config.assets.precompile += %w( bootstrap/glyphicons-halflings-regular.svg )
-Rails.application.config.assets.precompile += %w( bootstrap/glyphicons-halflings-regular.ttf )
-Rails.application.config.assets.precompile += %w( bootstrap/glyphicons-halflings-regular.woff )
-Rails.application.config.assets.precompile += %w( bootstrap/glyphicons-halflings-regular.woff2 )
+
