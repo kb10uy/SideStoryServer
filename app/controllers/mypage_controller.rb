d69fb86e507d92ce7b0b4ca44bf02c0d7045f44d
kb10uy <kb10uy@live.jp>
2015-05-11 00:33:30 +0900

    マイページに自分の投稿ぐらいは表示するようにした それから検索をちょっとだけ強化

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,5 +1,16 @@
 class MypageController < ApplicationController
   before_action :authenticate_user!
+  
   def index
+    @my_posts = Post.where(user: current_user).page(params[:page]).per(10)
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
+  
+  def myposts
+    
+  end
+  
 end
